497ca97930a5b10b0e6cf7efa203603cab856990
tuanviet97 <tuanviet97@users.noreply.github.com>
2019-02-28 14:28:38 +0700

    l
    l

---
+++ indexjs.js
@@ -0,0 +1,12 @@
+function about(){
+var divabout = document.getElementById("aboutme");
+ if(divabout.style.display == "none"){
+    divabout.style.display = "flex";
+    divabout.style.justifyContent ="center";
+ }else{
+
+    divabout.style.display ="none"; 
+ }
+
+
+}
