83adadeb5b376ac97ef6e61168de641934a61f01
tuanviet97 <tuanviet97@users.noreply.github.com>
2019-03-03 23:34:25 +0700

    s
    s

--- indexjs.js
+++ indexjs.js
@@ -1,12 +1,17 @@
+
+window.onload = function(){
+   var about2 = document.getElementById("about");
+   about2.addEventListener("click",about);
+   var divabout = document.getElementById("aboutme");
+   divabout.style.display = "none" ;
+}
+
 function about(){
 var divabout = document.getElementById("aboutme");
- if(divabout.style.display == "none"){
+ if(!divabout.style.display== "none" || divabout.style.display == "none"){
     divabout.style.display = "flex";
     divabout.style.justifyContent ="center";
- }else{
-
+ }else {
     divabout.style.display ="none"; 
  }
-
-
 }
